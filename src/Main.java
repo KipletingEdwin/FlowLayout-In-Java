12d1c23904024945d4056d76e83a8b4df4dd9d36
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-04 12:30:38 +0300

    create a panel and add all the buttons to it

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,24 @@ public class Main {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
 //        frame.setVisible(true);
 
-        frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton("3"));
-        frame.add(new JButton("4"));
-        frame.add(new JButton("5"));
-        frame.add(new JButton("6"));
-        frame.add(new JButton("7"));
-        frame.add(new JButton("8"));
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(250,250));
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(new FlowLayout());
+
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+
+        frame.add(panel);
 
         frame.setVisible(true);
 
